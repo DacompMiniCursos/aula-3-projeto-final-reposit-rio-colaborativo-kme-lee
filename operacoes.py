17d5b82a9b0e2cb3e032f0d687146f2eeadc9ea3
mileenacm <vibesstudy2@gmail.com>
2026-05-25 10:18:59 -0300

    feat: adicionando variavel na subtração

---
+++ operacoes.py
@@ -0,0 +1,17 @@
+def multiplicar(a,b):
+    return a * b
+
+def divisão(a,b):
+
+    menor = a
+    if menor>b:
+        menor = b
+        return print("b é menor que a")
+    
+    return a/b
+
+def subtração(a,b):
+    return a - b
+
+def adição(a,b):
+    return a + b
